8905ab5316c8b0c2b20e4f161815b78c7d416732
Lewis Dean <lewis.dean@datasum.app>
2022-06-11 18:08:54 +0100

    Restructured React Folder

--- oss-asset-tracker/App.js
+++ oss-asset-tracker/App.js
@@ -1,8 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-import AssetTagScanning from './components/asset-tag-scanning';
-import styles from './style/styles';
+import AssetTagScanning from './src/views/asset-tag-scanning';
+import styles from './src/style/styles';
 
 export default function App() {
   return (
