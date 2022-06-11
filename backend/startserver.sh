ac6d03c1519bb6e35cda09a77b72df5152baa238
Lewis Dean <lewis.dean@datasum.app>
2022-06-11 13:28:38 +0100

    Moved to postgres

---
+++ backend/startserver.sh
@@ -0,0 +1,3 @@
+python3 ./web/manage.py makemigrations assets
+python3 ./web/manage.py migrate 
+python3 ./web/manage.py runserver 0.0.0.0:8000
--- backend/web/asset_scanning/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AssetScanningConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'asset_scanning'
